f4cd86080a06ca4f629c09e8a6b8ba5ef0ef2b05
Willian <willsbit@gmail.com>
2022-06-16 20:52:01 -0300

    [FEATURE] Make the program multi-threaded

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -60,7 +60,7 @@ fn pixel_to_point(bounds: (usize, usize),
 
     Complex {
         re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-        im: upper_left.im + pixel.1 as f64 * height / bounds.1 as f64
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
         // Why subtraction here? pixel.1 increases as we go down,
         // but the imaginary component increases as we go up.
     }
@@ -142,8 +142,31 @@ fn main() {
     let lower_right = parse_complex(&args[4])
         .expect("error parsing lower right corner point");
 
-    let mut pixels - vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels
+            .chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(
+                    bounds,(0, top), upper_left, lower_right);
+
+                let band_lower_right = pixel_to_point(
+                    bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        }).unwrap();
+    }
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
