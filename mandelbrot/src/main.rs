ac523cd37a571f6bf36308c8f56deb7ced329633
Willian <willsbit@gmail.com>
2022-06-16 19:41:47 -0300

    [FEATURE] Add main function to parse CLI arguments and run the program

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -4,6 +4,7 @@ use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
 use std::io;
+use std::env;
 // The Mandelbrot set is defined as the set of complex numbers c
 // for which z does not fly out to infinity.
 
@@ -126,6 +127,25 @@ fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<
 
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1.0,0.20", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels - vec![0; bounds.0 * bounds.1];
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 #[cfg(test)]
